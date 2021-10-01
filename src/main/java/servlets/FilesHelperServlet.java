361f8f9716c6009de6a2b03b50354561afb45099
M <mihail.petuxov@yandex.ru>
2021-10-01 18:15:45 +0500

    Рефактор сервлета файлов

--- src/main/java/servlets/FilesHelperServlet.java
+++ src/main/java/servlets/FilesHelperServlet.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import models.MyFile;
+import models.UserProfile;
+import services.AccountService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -19,40 +24,47 @@ import java.util.stream.Collectors;
 public class FilesHelperServlet extends HttpServlet {
 
     Comparator<MyFile> fileDirComparator;
-    File[] roots;
-    String firstRootName;
 
     @Override
     public void init() throws ServletException {
         super.init();
         fileDirComparator = initComparator();
-        roots = File.listRoots();
-        firstRootName = roots[0].getAbsolutePath();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Map<String, String[]> params = req.getParameterMap();
+        UserProfile user = AccountService.getUserBySessionId(req.getSession().getId());
+        String path = params.getOrDefault("path", new String[]{""})[0];
 
-        String path = params.getOrDefault("path", new String[]{firstRootName})[0];
-        File file = new File(path);
-        List<MyFile> files = Arrays.stream(file.listFiles())
+        File dirFile = path.contains(user.getLogin()) ? new File(path) : getUserDirFile(user.getLogin());
+
+        List<MyFile> files = getSortedFiles(dirFile);
+
+        req.setAttribute("files", files);
+        req.setAttribute("currentFolder", dirFile.getAbsolutePath());
+        req.setAttribute("parentFolder", dirFile.getParent());
+        req.setAttribute("fileDate", new MyFile(dirFile).formatDate());
+        getServletContext().getRequestDispatcher("/view/files.jsp").forward(req, resp);
+    }
+
+    private List<MyFile> getSortedFiles(File dirFile) {
+        return Arrays.stream(dirFile.listFiles())
                 .map(MyFile::new)
                 .sorted(this.fileDirComparator)//.thenComparing(fieldComparator("size", true)))
                 .collect(Collectors.toList());
-
-        req.setAttribute("roots", this.roots);
-        req.setAttribute("files", files);
-        req.setAttribute("currentFolder", file.getAbsolutePath());
-        req.setAttribute("parentFolder", file.getParent());
-        req.setAttribute("fileDate", new MyFile(file).formatDate());
-        getServletContext().getRequestDispatcher("/view/files.jsp").forward(req, resp);
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/view/files.jsp").forward(req, resp);
+    private File getUserDirFile(String login){
+        String path = String.join(File.separator, System.getProperty("user.home"), "user files", login);
+        File file = new File(path);
+        if (!file.exists()){
+            file.mkdirs();
+        }
+        return file;
     }
+
+
 
     private Comparator<MyFile> initComparator() {
         return (a, b) -> a.getFile().isDirectory() && !b.getFile().isDirectory() ? -1 :
